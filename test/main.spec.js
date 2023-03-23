d045e9d8dc5a8eea13d765c486f7ebbaa82438a2
Fernanda <mfsepulveda@uc.cl>
2023-03-23 14:55:00 -0300

    chore(): add boilerplate

---
+++ test/main.spec.js
@@ -0,0 +1,12 @@
+import { example } from '../src/main.js';
+
+
+describe('example', () => {
+  it('is a function', () => {
+    expect(typeof example).toBe('function');
+  });
+
+  it('returns `example`', () => {
+    expect(example()).toBe('example');
+  });
+});
